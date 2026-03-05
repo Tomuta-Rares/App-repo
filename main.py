8e92f67561226548bfe927a737cfb80080a0bcdb
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-03-05 10:49:32 +0200

    modified the return message

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def create_item(item: ItemCreate):
     db.commit()
     db.refresh(db_item)
     db.close()
-    return {"message": "salut leo", "item": db_item}
+    return {"message": "salut victor", "item": db_item}
 
 @app.get("/items")
 def get_items():
