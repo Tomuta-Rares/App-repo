f21e9ba00906bbc1a6379e1a97545de3629390a1
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-04-01 10:18:25 +0300

    modified code for browser login

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,37 @@
+const keycloak = new Keycloak({
+    url: "https://auth.local:8443/auth",
+    realm: "devops-lvlup",
+    clientId: "shopping-frontend"
+});
+
+async function initKeycloak() {
+    const authenticated = await keycloak.init({
+        onLoad: "login-required",
+        checkLoginIframe: false
+    });
+
+    if (!authenticated) {
+        console.log("Not authenticated");
+        return;
+    }
+
+    console.log("Authenticated!");
+    console.log("User:", keycloak.tokenParsed.preferred_username);
+}
+
 async function loadItems() {
-    const res = await fetch("/api/items");
-    const items = await res.json();
+    const res = await fetch("/api/items", {
+        headers: {
+            Authorization: "Bearer " + keycloak.token
+        }
+    });
+
+    const data = await res.json();
 
     const list = document.getElementById("items");
     list.innerHTML = "";
 
-    items.forEach(i => {
+    data.items.forEach(i => {
         const li = document.createElement("li");
         li.textContent = i.name;
         list.appendChild(li);
@@ -17,11 +43,17 @@ async function createItem() {
 
     await fetch("/api/items", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + keycloak.token
+        },
         body: JSON.stringify({ name })
     });
 
     loadItems();
 }
 
-loadItems();
+// INIT APP
+initKeycloak().then(() => {
+    loadItems();
+});
